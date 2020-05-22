28fe7b026eb5797644365a3fd138b88611dd1cfd
Kalil Teixeira Ventura Monteiro <kalilventur@hotmail.com>
2020-05-22 17:12:52 -0300

    Criando o formulario para uma nova tarefa

--- Frontend/Todo/src/app/pages/new/new.component.ts
+++ Frontend/Todo/src/app/pages/new/new.component.ts
@@ -1,4 +1,7 @@
+import { Router } from '@angular/router';
+import { DataService } from './../../data.service';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new',
@@ -6,10 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new.component.css']
 })
 export class NewComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private serivce: DataService,
+    private router: Router
+  ) {
+    this.form = this.fb.group({
+      title: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.maxLength(60),
+        Validators.required,
+      ])],
+      date: [new Date().toJSON().substring(0, 10), Validators.required]
+    });
+  }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  submit() {
+    this.serivce.postTodo(this.form.value, localStorage.getItem('token')).subscribe(res => {
+      this.router.navigateByUrl('/');
+    }, err => console.error(err));
   }
 
 }
