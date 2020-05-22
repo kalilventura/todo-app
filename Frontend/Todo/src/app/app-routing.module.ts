8867e4d386899a2fce1e82f5d8e93f27b61e3fbc
Kalil Teixeira Ventura Monteiro <kalilventur@hotmail.com>
2020-05-22 14:27:34 -0300

    Pagina de login

--- Frontend/Todo/src/app/app-routing.module.ts
+++ Frontend/Todo/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
+import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: LoginComponent }
 ];
 
 @NgModule({
